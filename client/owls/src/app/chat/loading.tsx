104d6da0421f4634dec09b0fa8eb78e3dc51bf4c
devhindo <lildevhind@gmail.com>
2023-11-11 04:32:38 +0200

    added landpage and loading page

---
+++ client/owls/src/app/chat/loading.tsx
@@ -0,0 +1,12 @@
+
+import { FC } from 'react';
+
+const Loading: FC = () => {
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
--- client/owls/src/app/page.tsx
+++ client/owls/src/app/page.tsx
@@ -2,12 +2,15 @@ import Link from 'next/link'
 import styles from './page.module.css'
 
 
+
 export default function Home() {
   return (
-    <main>
-      <Link href="/chat">
+    <div>
+    <main className={styles.main}>
+      <Link href="/chat" className={styles.chat}>
         chat
       </Link>
     </main>
+    </div>
   )
 }
